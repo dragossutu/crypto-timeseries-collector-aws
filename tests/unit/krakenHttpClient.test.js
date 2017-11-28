5e9322aff076772ee896a24768146eb2bc11dff2
Dragos Sutu <dragos.sutu@gmail.com>
2017-11-28 07:51:49 +0200

    Extract duplicate code.

--- tests/unit/krakenHttpClient.test.js
+++ tests/unit/krakenHttpClient.test.js
@@ -1,37 +1,45 @@
 'use strict';
 
+const url = 'dummy.url';
+const pair = 'XBTEUR';
+const result = {"ohlcv": "data"};
+
+function createHttpsMock(apiResponseString) {
+    const httpMock = require.requireMock('http');
+    /** @type {ClientRequest} */
+    const clientRequestMock = httpMock.ClientRequest;
+    clientRequestMock.on = jest.fn(() => {
+        return clientRequestMock;
+    });
+    clientRequestMock.end = jest.fn();
+
+    /** @type {IncomingMessage} */
+    const responseMock = httpMock.IncomingMessage;
+    responseMock.on = jest.fn((event, cb) => {
+        if ('data' === event) {
+            cb(apiResponseString);
+        } else if ('end' === event) {
+            cb();
+        }
+
+        return responseMock;
+    });
+
+    const httpsMock = require.requireMock('https');
+    httpsMock.get = jest.fn((options, cb) => {
+        cb(responseMock);
+        return clientRequestMock;
+    });
+
+    return httpsMock;
+}
+
+
 describe('fetchData method', () => {
     test('callback is called without error and with data when api response is successful', done => {
-        const url = 'dummy.url';
-        const pair = 'XBTEUR';
-        const result = {"ohlcv": "data"};
         const apiResponseString = '{"error": "", "result": { "XXBTZEUR": ' + JSON.stringify(result) + '}}';
 
-        const httpMock = require.requireMock('http');
-        /** @type {ClientRequest} */
-        const clientRequestMock = httpMock.ClientRequest;
-        clientRequestMock.on = jest.fn(() => {
-            return clientRequestMock;
-        });
-        clientRequestMock.end = jest.fn();
-
-        /** @type {IncomingMessage} */
-        const responseMock = httpMock.IncomingMessage;
-        responseMock.on = jest.fn((event, cb) => {
-            if ('data' === event) {
-                cb(apiResponseString);
-            } else if ('end' === event) {
-                cb();
-            }
-
-            return responseMock;
-        });
-
-        const httpsMock = require.requireMock('https');
-        httpsMock.get = jest.fn((options, cb) => {
-            cb(responseMock);
-            return clientRequestMock;
-        });
+        const httpsMock = createHttpsMock(apiResponseString);
 
         /** @type {KrakenHttpClient} */
         const sut = require('../../src/krakenHttpClient.js')(httpsMock);
@@ -48,38 +56,11 @@ describe('fetchData method', () => {
     });
 
     test('callback is called with error when api response contains at least one error', done => {
-        const url = 'dummy.url';
-        const pair = 'XBTEUR';
-        const result = {"ohlcv": "data"};
         const apiResponseString = '{"error": ["some error"], "result": { "XXBTZEUR": ' + JSON.stringify(result) + '}}';
 
-        const httpMock = require.requireMock('http');
-        /** @type {ClientRequest} */
-        const clientRequestMock = httpMock.ClientRequest;
-        clientRequestMock.on = jest.fn(() => {
-            return clientRequestMock;
-        });
-        clientRequestMock.end = jest.fn();
+        const httpsMock = createHttpsMock(apiResponseString);
 
-        /** @type {IncomingMessage} */
-        const responseMock = httpMock.IncomingMessage;
-        responseMock.on = jest.fn((event, cb) => {
-            if ('data' === event) {
-                cb(apiResponseString);
-            } else if ('end' === event) {
-                cb();
-            }
-
-            return responseMock;
-        });
-
-        const httpsMock = require.requireMock('https');
-        httpsMock.get = jest.fn((options, cb) => {
-            cb(responseMock);
-            return clientRequestMock;
-        });
-
-        const errors = {
+        const expectedError = {
             'requestUrl': url + '/0/public/OHLC?pair=' + pair,
             'response': JSON.parse(apiResponseString)
         };
@@ -90,7 +71,7 @@ describe('fetchData method', () => {
         const callback = jest.fn((error, data) => {
             expect(httpsMock.get).toHaveBeenCalledTimes(1);
             expect(callback).toHaveBeenCalledTimes(1);
-            expect(callback).toBeCalledWith(errors);
+            expect(callback).toBeCalledWith(expectedError);
 
             done();
         });
