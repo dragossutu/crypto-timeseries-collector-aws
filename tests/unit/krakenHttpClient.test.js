eafc59a5bdb6c6ddb6f683cc260125f906b06ba3
Dragos Sutu <dragos.sutu@gmail.com>
2017-11-28 08:07:13 +0200

    Add http communication error case test, attempt to make code more readable.

--- tests/unit/krakenHttpClient.test.js
+++ tests/unit/krakenHttpClient.test.js
@@ -4,14 +4,13 @@ const url = 'dummy.url';
 const pair = 'XBTEUR';
 const result = {"ohlcv": "data"};
 
-function createHttpsMock(apiResponseString) {
+/**
+ * @param {string} apiResponseString
+ * @param {string} httpError
+ * @returns {Object}
+ */
+function createHttpsMock(apiResponseString, httpError = '') {
     const httpMock = require.requireMock('http');
-    /** @type {ClientRequest} */
-    const clientRequestMock = httpMock.ClientRequest;
-    clientRequestMock.on = jest.fn(() => {
-        return clientRequestMock;
-    });
-    clientRequestMock.end = jest.fn();
 
     /** @type {IncomingMessage} */
     const responseMock = httpMock.IncomingMessage;
@@ -25,9 +24,23 @@ function createHttpsMock(apiResponseString) {
         return responseMock;
     });
 
+    /** @type {ClientRequest} */
+    const clientRequestMock = httpMock.ClientRequest;
+    clientRequestMock.on = jest.fn((event, cb) => {
+        if (httpError && 'error' === event) {
+            cb(httpError);
+        }
+
+        return clientRequestMock;
+    });
+    clientRequestMock.end = jest.fn();
+
     const httpsMock = require.requireMock('https');
     httpsMock.get = jest.fn((options, cb) => {
-        cb(responseMock);
+        if (!httpError) {
+            cb(responseMock);
+        }
+
         return clientRequestMock;
     });
 
@@ -72,6 +85,26 @@ describe('fetchData method', () => {
             expect(httpsMock.get).toHaveBeenCalledTimes(1);
             expect(callback).toHaveBeenCalledTimes(1);
             expect(callback).toBeCalledWith(expectedError);
+
+            done();
+        });
+
+        sut.fetchData(url, pair, callback);
+    });
+
+    test('callback is called with error when a communication error occurs', done => {
+        const apiResponseString = '{"error": "", "result": { "XXBTZEUR": ' + JSON.stringify(result) + '}}';
+        const expectedHttpError = 'Http error';
+
+        const httpsMock = createHttpsMock(apiResponseString, expectedHttpError);
+
+        /** @type {KrakenHttpClient} */
+        const sut = require('../../src/krakenHttpClient.js')(httpsMock);
+
+        const callback = jest.fn((error, data) => {
+            expect(httpsMock.get).toHaveBeenCalledTimes(1);
+            expect(callback).toHaveBeenCalledTimes(1);
+            expect(callback).toBeCalledWith(expectedHttpError);
 
             done();
         });
